2540746f0232176322fec67f77fb387c8d7c0c17
Christian Spada <spada17@gmail.com>
2023-05-10 14:06:28 -0400

    finish express set up

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const routes = require('./routes');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -14,22 +15,21 @@ const app = express();
 // log info about requests
 app.use(morgan('dev'));
 
+// === Parsing Middleware ===
 app.use(cookieParser());
 app.use(express.json());
 
-// Security Middleware
+// === Security Middleware ==
 if (!isProduction) {
 	// enable cors only in development
 	app.use(cors());
 }
-
 // helmet helps set a variety of headers to better secure your app
 app.use(
 	helmet.crossOriginResourcePolicy({
 		policy: 'cross-origin',
 	})
 );
-
 // Set the _csrf token and create req.csrfToken method
 app.use(
 	csurf({
@@ -40,3 +40,8 @@ app.use(
 		},
 	})
 );
+
+// connect routes
+app.use(routes);
+
+module.exports = app;
