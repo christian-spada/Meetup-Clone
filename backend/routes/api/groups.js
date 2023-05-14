f7c0120d0322cdcdc0fdf3db1bffbc88cf0b9cde
Christian Spada <spada17@gmail.com>
2023-05-13 20:59:23 -0400

    in progress: work on get all groups endpoint

---
+++ backend/routes/api/groups.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const { requireAuth } = require('../../utils/auth.js');
+const { Group, Membership, GroupImage } = require('../../db/models');
+const { toJSONArray } = require('../../utils/helpers.js');
+
+router.get('/', async (req, res) => {
+	const groups = await Group.findAll({
+		include: {
+			model: GroupImage,
+			where: {
+				preview: true,
+			},
+			attributes: ['url'],
+			required: false,
+		},
+	});
+
+	const groupArr = [];
+	for (const group of groups) {
+		const groupObj = group.toJSON();
+		const numMembers = await Membership.count({
+			where: {
+				groupId: group.id,
+			},
+		});
+
+		const url = groupObj.GroupImages[0]?.url;
+		groupObj.previewImage = url || null;
+		groupObj.numMembers = numMembers;
+		delete groupObj.GroupImages;
+
+		groupArr.push(groupObj);
+	}
+
+	res.json(groupArr);
+});
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const groupsRouter = require('./groups.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // If current user session is valid, set req.user to the user in the database
@@ -8,6 +9,7 @@ const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/groups', groupsRouter);
 
 router.post('/test', async (req, res) => {
 	res.json({ requestBody: req.body });
