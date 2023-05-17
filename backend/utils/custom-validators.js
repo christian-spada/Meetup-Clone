d86c7f8fe63307dfb45f7f64b7ea6324f7f5b872
Christian Spada <spada17@gmail.com>
2023-05-17 09:28:27 -0400

    create custom-validators.js util

---
+++ backend/utils/custom-validators.js
@@ -0,0 +1,26 @@
+const { handleValidationErrors } = require('./validation');
+const { check } = require('express-validator');
+
+// === CREATE A GROUP ===
+const validateGroupCreation = [
+	check('name').isLength({ min: 1, max: 60 }).withMessage('Name must be 60 characters or less'),
+	check('about').isLength({ min: 50 }).withMessage('About must be 50 characters or more'),
+	check('type').isIn(['Online', 'In person']).withMessage("Type must be 'Online' or 'In person'"),
+	check('private').isBoolean().withMessage('Private must be a boolean'),
+	check('city').exists({ checkFalsy: true }).withMessage('City is required'),
+	check('state').exists({ checkFalsy: true }).withMessage('State is required'),
+	handleValidationErrors,
+];
+
+// === EDIT A GROUP ===
+const validateGroupEdit = [
+	check('name').isLength({ min: 1, max: 60 }).withMessage('Name must be 60 characters or less'),
+	check('about').isLength({ min: 50 }).withMessage('About must be 50 characters or more'),
+	check('type').isIn(['Online', 'In person']).withMessage("Type must be 'Online' or 'In person'"),
+	check('private').isBoolean().withMessage('Private must be a boolean'),
+	check('city').exists({ checkFalsy: true }).withMessage('City is required'),
+	check('state').exists({ checkFalsy: true }).withMessage('State is required'),
+	handleValidationErrors,
+];
+
+module.exports = { validateGroupCreation, validateGroupEdit };
