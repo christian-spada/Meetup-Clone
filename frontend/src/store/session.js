7c0193a1a408f88009ee7f54ffdd74f55f418a2d
Christian Spada <spada17@gmail.com>
2023-06-02 10:34:25 -0400

    implement restore user functionality and finish phase-1

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,13 @@ export const loginThunk = user => async dispatch => {
 	return response;
 };
 
+export const restoreUserThunk = () => async dispatch => {
+	const response = await csrfFetch('/api/session');
+	const data = await response.json();
+	dispatch(setUser(data.user));
+	return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
