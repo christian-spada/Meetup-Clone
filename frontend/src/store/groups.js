399358b98812bbbe1b6f3dac70c88b6e8fbae332
Christian Spada <spada17@gmail.com>
2023-06-06 12:43:40 -0400

    remove create group and add image actions

--- frontend/src/store/groups.js
+++ frontend/src/store/groups.js
@@ -4,8 +4,7 @@ import { normalizeData } from './storeUtils';
 // === ACTIONS ===
 
 const GET_ALL_GROUPS = 'groups/getAllGroups';
-const CREATE_GROUP = 'groups/createGroup';
-const ADD_GROUP_IMAGE = 'groups/addGroupImage';
+// const CREATE_GROUP = 'groups/createGroup';
 
 const getAllGroups = groups => {
 	return {
@@ -14,19 +13,12 @@ const getAllGroups = groups => {
 	};
 };
 
-const createGroup = group => {
-	return {
-		type: CREATE_GROUP,
-		payload: group,
-	};
-};
-
-const addGroupImage = image => {
-	return {
-		type: ADD_GROUP_IMAGE,
-		payload: image,
-	};
-};
+// const createGroup = group => {
+// 	return {
+// 		type: CREATE_GROUP,
+// 		payload: group,
+// 	};
+// };
 
 // === THUNKS ===
 export const getAllGroupsThunk = () => async dispatch => {
@@ -45,19 +37,11 @@ export const createGroupThunk = (group, image) => async dispatch => {
 
 	if (res.ok) {
 		const newGroup = await res.json();
-		console.log('newGroup', newGroup);
-		console.log('image', image);
 
-		const imgRes = await csrfFetch(`/api/groups/${newGroup.id}/images`, {
+		await csrfFetch(`/api/groups/${newGroup.id}/images`, {
 			method: 'POST',
 			body: JSON.stringify(image),
 		});
-
-		const img = await imgRes.json();
-
-		newGroup.groupImages = [img];
-
-		dispatch(createGroup(newGroup));
 
 		return newGroup;
 	}
@@ -69,26 +53,10 @@ const initialState = {};
 
 const groupsReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case CREATE_GROUP:
-			return {
-				...state,
-				allGroups: {
-					...state.allGroups,
-					[action.payload.id]: action.payload,
-				},
-			};
 		case GET_ALL_GROUPS:
 			return {
 				...state,
 				allGroups: normalizeData(action.payload),
-			};
-		case ADD_GROUP_IMAGE:
-			return {
-				...state,
-				singleGroup: {
-					...state.singleGroup,
-					GroupImages: [...state.singleGroup.GroupImages, action.payload],
-				},
 			};
 		default:
 			return state;
