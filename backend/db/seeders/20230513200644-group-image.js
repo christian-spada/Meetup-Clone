0fbfaffce58b3325e45b3c30f4f1d7ffe8925c2d
Christian Spada <spada17@gmail.com>
2023-06-10 15:37:10 -0400

    add real image urls to the event/group images seeder files

--- backend/db/seeders/20230513200644-group-image.js
+++ backend/db/seeders/20230513200644-group-image.js
@@ -10,33 +10,33 @@ options.tableName = 'GroupImages';
 const groupImagesData = [
 	{
 		groupId: 1,
-		url: '/pic',
+		url: 'https://cdn.discordapp.com/attachments/1116875350929068035/1117171632776167505/aldebaran-s-uXchDIKs4qI-unsplash.jpg',
 		preview: true,
 	},
 	{
 		groupId: 2,
-		url: '/pic',
+		url: 'https://cdn.discordapp.com/attachments/1116875350929068035/1117171633082343534/guillermo-ferla-Oze6U2m1oYU-unsplash.jpg',
 		preview: true,
 	},
 	{
 		groupId: 3,
-		url: '/pic',
-		preview: false,
+		url: 'https://cdn.discordapp.com/attachments/1116875350929068035/1117171633451438120/ivana-cajina-asuyh-_ZX54-unsplash.jpg',
+		preview: true,
 	},
 	{
 		groupId: 4,
-		url: '/pic',
+		url: 'https://cdn.discordapp.com/attachments/1116875350929068035/1117171633824743515/jeremy-thomas-E0AHdsENmDg-unsplash.jpg',
 		preview: true,
 	},
 	{
 		groupId: 5,
-		url: '/pic',
+		url: 'https://cdn.discordapp.com/attachments/1116875350929068035/1117171635506647212/vincentiu-solomon-ln5drpv_ImI-unsplash.jpg',
 		preview: true,
 	},
 	{
 		groupId: 6,
-		url: '/pic',
-		preview: false,
+		url: 'https://cdn.discordapp.com/attachments/1116875350929068035/1117171634869125160/shot-by-cerqueira-0o_GEzyargo-unsplash.jpg',
+		preview: true,
 	},
 ];
 
